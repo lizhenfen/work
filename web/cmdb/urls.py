e73e3baf6def7ff9a59867afb00568f2713beab5
lizhenfen <743564797@qq.com>
2017-02-22 14:14:23 +0800

    delete

--- web/cmdb/urls.py
+++
@@ -1,8 +0,0 @@
-from __future__ import  absolute_import
-from django.conf.urls import url
-from . import  views
-app_name = 'cmdb'
-urlpatterns = [
-    url(r'^$', views.home),
-    url(r'^servers/$', views.servers, name='servers'),
-]
--- web/web/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-from index import views
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^cmdb/', include('cmdb.urls'), name='cmdb'),
-    url(r'^$', views.index, name="index")
-]
